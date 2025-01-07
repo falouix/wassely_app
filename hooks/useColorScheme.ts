bef43a7566cf1be2c54ea8a0e7e3300c82a0aab2
fakhreddine <falouix@gmail.com>
2025-01-07 13:55:18 +0100

    Add contents of dist folder to dev_publish branch

--- hooks/useColorScheme.ts
+++
@@ -1,6 +0,0 @@
-// components/useColorScheme.ts
-import { useColorScheme as useRNColorScheme } from 'react-native';
-
-export function useColorScheme() {
-  return useRNColorScheme() || 'light'; // Fallback to 'light' if undefined
-}
